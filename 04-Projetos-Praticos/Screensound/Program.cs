f616b536f9e37138ad137a6a13f22a2d3bd34e5f
Amanda <aaamnd112@gmail.com>
2026-02-11 09:29:20 -0300

    feat: criar a funcao mostrarbandasregistradas

--- 04-Projetos-Praticos/Screensound/Program.cs
+++ 04-Projetos-Praticos/Screensound/Program.cs
@@ -1,5 +1,5 @@
 ﻿string mensagemDeBoasVindas = "Bem-vindo ao Screensound!";
-List<string> listaDasBandas = new List<string> { "Pink Floyd", "Led Zeppelin", "The Beatles", "Queen", "Nirvana" };
+List<string> listaDasBandas = new List<string> { "U2", "The Beatles", "Calypso" };
 
 void ExibirLogo()
 {
@@ -32,10 +32,8 @@ void ExibirOpcoesDoMenu()
     switch (opcaoEscolhidaNumerica)
     {
         case 1: RegistrarBanda();
-            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
             break;
-        case 2:
-            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+        case 2: MostrarBandasRegistradas();
             break;
         case 3:
             Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
@@ -55,11 +53,32 @@ void ExibirOpcoesDoMenu()
 void RegistrarBanda()
 {
     Console.Clear();
+    Console.WriteLine("*********************");
     Console.WriteLine("Registro de bandas");
+    Console.WriteLine("*********************\n");
     Console.Write("Digite o nome da banda que deseja registrar: ");
     string nomeDaBanda = Console.ReadLine()!;
+    listaDasBandas.Add(nomeDaBanda);
     Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
     Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+void MostrarBandasRegistradas()
+{
+    Console.Clear();
+    Console.WriteLine("***************************************");
+    Console.WriteLine("Exibindo todas as bandas registradas");
+    Console.WriteLine("***************************************");
+
+    for (int i = 0; i < listaDasBandas.Count; i++)
+    {
+        Console.WriteLine($"Banda: {listaDasBandas[i]}");
+    }
+
+    Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+    Console.ReadKey();
     Console.Clear();
     ExibirOpcoesDoMenu();
 }
