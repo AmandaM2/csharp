861c7e9a2d1ac821e3906c11cf70f7ca93b5a6d3
Amanda <aaamnd112@gmail.com>
2026-02-23 11:47:50 -0300

    refactor: migrar armazenamento de bandas para Dicionario

--- 04-Projetos-Praticos/Screensound/Program.cs
+++ 04-Projetos-Praticos/Screensound/Program.cs
@@ -1,5 +1,8 @@
 ﻿string mensagemDeBoasVindas = "Bem-vindo ao Screensound!";
-List<string> listaDasBandas = new List<string> { "U2", "The Beatles", "Calypso" };
+//List<string> listaDasBandas = new List<string> { "U2", "The Beatles", "Calypso" };
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+bandasRegistradas.Add("Linkin Park", new List<int> { 10,8,6});
+bandasRegistradas.Add("The Beatles", new List<int> ());
 
 void ExibirLogo()
 {
@@ -56,7 +59,7 @@ void RegistrarBanda()
     ExibirTituloDaOpcao("Registro das bandas");
     Console.Write("Digite o nome da banda que deseja registrar: ");
     string nomeDaBanda = Console.ReadLine()!;
-    listaDasBandas.Add(nomeDaBanda);
+    bandasRegistradas.Add(nomeDaBanda, new List<int> ( ));
     Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
     Thread.Sleep(2000);
     Console.Clear();
@@ -68,7 +71,7 @@ void MostrarBandasRegistradas()
     Console.Clear();
     ExibirTituloDaOpcao("Exibindo todas as bandas registradas");
 
-    foreach (string banda in listaDasBandas)
+    foreach (string banda in bandasRegistradas.Keys) 
     {
         Console.WriteLine($"Banda: {banda}");
     }
